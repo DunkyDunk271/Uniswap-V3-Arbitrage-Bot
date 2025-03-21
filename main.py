91e100f316c6d0ceaa645ccec1a1f0c2abeb3332
DunkyDunk271 <nqd2701@gmail.com>
2025-03-21 17:37:13 +0700

    .

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import json
 import websockets
 import asyncio
 from web3 import Web3
+import nest_asyncio
+
+nest_asyncio.apply()
 
 GRAPHQL_URL = "https://gateway.thegraph.com/api/8eb05dafcde7a82f664adace07cb1437/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV"
 QUICKNODE_RPC = "https://yolo-aged-darkness.quiknode.pro/401a21cac95f67e72bb1478cf94b4ff0763535cc/"
@@ -18,7 +21,7 @@ UNISWAP_V3_ROUTER = "0xe592427a0aece92de3edee1f18e0157c05861564"
 #UNISWAP_V3_ROUTER = "0xb41b78Ce3D1BDEDE48A3d303eD2564F6d1F6fff0"
 
 async def main():
-    PoolList = process_pools()
+    #PoolList = await process_pools()
 
     async with websockets.connect(CURRENT_WSS, ping_interval = 30) as websocket:
         subscribe_payload = {
@@ -37,34 +40,8 @@ async def main():
             data = json.loads(message)
             swap_transaction = CheckSwapTransaction(data)
             
-            if swap_transaction == None:
+            if swap_transaction is None:
                 continue
             
-
-
-    '''
-
-    LiquidityPoolIDs = [Uni_WETH_ID, Uni_USDT_ID, Sushi_WETH_ID, Sushi_USDT_ID]
-
-    PendingTransactions = ReadMempool()
-    
-    PendingTransactions = PotentialNextBlock(PendingTransactions)
-
-    PendingTransactions = TransactionFilter(PendingTransactions, LiquidityPoolIDs)
-
-    Sushi_WETH_Total_Supply, Sushi_USDT_Total_Supply, Uni_WETH_Total_Supply, Uni_USDT_Total_Supply = ReserveEstimation(PendingTransactions, LiquidityPoolIDs, Uni_USDT_Total_Supply, Uni_WETH_Total_Supply, Sushi_USDT_Total_Supply, Sushi_WETH_Total_Supply)
-
-    Opportunity = ArbitrageOpportunitySpot(Sushi_WETH_Total_Supply, Sushi_USDT_Total_Supply, Uni_WETH_Total_Supply, Uni_USDT_Total_Supply)
-    if Opportunity == "UniWETH-USDT/SushiUSDT-WETH":
-        OptimalAmount, Profit = OptimalArbitrageAmount(Uni_WETH_Total_Supply, Uni_USDT_Total_Supply, Uni_USDT_Total_Supply, Uni_WETH_Total_Supply)
-    elif Opportunity == "UniUSDT-WETH/SushiWETH-USDT":
-        OptimalAmount, Profit = OptimalArbitrageAmount(Uni_USDT_Total_Supply, Uni_WETH_Total_Supply, Sushi_WETH_Total_Supply, Sushi_USDT_Total_Supply)
-    else:
-        print(Opportunity)
-    
-    print(f"Optimal Amount: {OptimalAmount}")
-    print(f"Profit: {Profit}")
-    '''
-
 if __name__ == "__main__":
     asyncio.run(main())
